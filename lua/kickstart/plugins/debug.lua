64f8bfce4c5627652dc813a25e3b8d8240e27e21
Chiew Yan Wei <chiewyanwei@gmail.com>
2023-06-13 13:37:23 +0800

    Set up DAP for Rust.

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -20,6 +20,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    -- 'simrat39/rust-tools.nvim',
   },
   config = function()
     local dap = require 'dap'
@@ -39,6 +40,8 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'codelldb',
+        'cpptools',
       },
     }
 
@@ -47,6 +50,7 @@ return {
     vim.keymap.set('n', '<F1>', dap.step_into)
     vim.keymap.set('n', '<F2>', dap.step_over)
     vim.keymap.set('n', '<F3>', dap.step_out)
+    vim.keymap.set('n', '<S-F5>', dap.terminate)
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -73,13 +77,82 @@ return {
       },
     }
     -- toggle to see last session result. Without this ,you can't see session output in case of unhandled exception.
-    vim.keymap.set("n", "<F7>", dapui.toggle)
-    
+    vim.keymap.set('n', '<F7>', dapui.toggle)
+
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
+    -- Use telescope for selecting executables to debug
+    local pickers = require 'telescope.pickers'
+    local finders = require 'telescope.finders'
+    local conf = require('telescope.config').values
+    local actions = require 'telescope.actions'
+    local action_state = require 'telescope.actions.state'
+    local builtin = require 'telescope.builtin'
+
     -- Install golang specific config
     require('dap-go').setup()
+
+    -- Install rust config
+    local mason_registry = require 'mason-registry'
+    local codelldb = mason_registry.get_package 'codelldb'
+    local extension_path = codelldb:get_install_path() .. '\\extension\\'
+    local codelldb_path = extension_path .. 'adapter\\codelldb'
+    local liblldb_path = extension_path .. 'lldb\\lib\\liblldb.lib'
+
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+        command = codelldb_path,
+        args = { '--port', '${port}' },
+
+        -- Might need to uncomment this on Windows:
+        -- detached = false,
+      },
+    }
+
+    dap.configurations.rust = {
+      {
+        name = 'Launch file',
+        type = 'codelldb',
+        request = 'launch',
+        -- program = function()
+        --   return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        -- end,
+        cwd = '${workspaceFolder}',
+        program = function()
+          return coroutine.create(function(coro)
+            local opts = {}
+            pickers
+              .new(opts, {
+                prompt_title = 'Path to executable',
+                finder = finders.new_oneshot_job({ 'fd', '--hidden', '--no-ignore', '--type', 'x' }, {}),
+                sorter = conf.generic_sorter(opts),
+                attach_mappings = function(buffer_number)
+                  actions.select_default:replace(function()
+                    actions.close(buffer_number)
+                    coroutine.resume(coro, action_state.get_selected_entry()[1])
+                  end)
+                  return true
+                end,
+              })
+              :find()
+          end)
+        end,
+        args = {
+          function()
+            return vim.fn.input 'Args: '
+          end,
+        },
+        -- stopOnEntry = false,
+      },
+    }
+    -- require('rust-tools').setup {
+    --   dap = {
+    --     adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path),
+    --   },
+    -- }
   end,
 }
